bb0f0d8c00f4b02c4bf12e0a08ec38f4d7555261
Guy Podjarny <guypod@users.noreply.github.com>
2020-06-29 09:57:14 +0100

    Demo code change
    To create a PR

--- routes/index.js
+++ routes/index.js
@@ -55,6 +55,7 @@ exports.admin = function (req, res, next) {
 function parse(todo) {
   var t = todo;
 
+  var unusedVar = 3;
   var remindToken = ' in ';
   var reminder = t.toString().indexOf(remindToken);
   if (reminder > 0) {
